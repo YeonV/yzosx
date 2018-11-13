cf3feefc62e7b1c22876ee659f967a39c7c9ccae
Yeon Vinzenz Varapragasam <YeonVinzenz.Varapragasam@trustedshops.de>
2018-11-13 12:06:45 +0100

    no message

--- yz.sh
+++ yz.sh
@@ -130,9 +130,9 @@ export CPPFLAGS="-I/usr/local/opt/ruby/include"
 echo "\033[1;31m================================================================================\033[0m"
 echo "\033[1;31m                            Installing tmuxinator ...\033[0m"
 echo "\033[1;31m--------------------------------------------------------------------------------\033[0m"
-curl -sSL https://get.rvm.io | bash -s stable
+curl -sSL https://get.rvm.io | sudo -u $loggedInUser bash -s stable
 # source ~/.rvm/scripts/rvm
-rvm install 2.4 --disable-binary
+sudo -u $loggedInUser rvm install 2.4 --disable-binary
 gem sources -r https://rubygems.org/
 gem sources --add https://rubygems.org
 sudo -u $loggedInUser gem install tmuxinator
